c190afff5d8c20672bb94c99a53626d406d11523
Vu Tran <baysao@gmail.com>
2017-08-03 13:21:59 +0700

    add exclude tables

--- backup_host.sh
+++ backup_host.sh
@@ -55,9 +55,11 @@ else
 	pass=`awk -F'=' '/password =/ {print $2}' $conf | tr -d \'\"`
 	port=`awk -F'=' '/port =/ {print $2}' $conf | grep 300 | tr -d \'\"`
 
+	#opt_exclude="^pw\d[.]\w+_archive_(blob|numeric)_temp_\w+"
+	opt_exclude="(_archive_(blob|numeric)_temp_|_log_link_visit_action_2)"
 	#datadir path is the directory on romote server while target-dir is dir on this bk server 
 	#so both target-dir and data-dir need to be the dirs on local host (or specify as host/path/file or smt
-	$xtrabackup --innodb-log-file-size=50331648 --backup -H $host -P $port -u $user -p $pass --datadir=$datadir --target-dir=$target 
+	$xtrabackup --tables-exclude=$opt_exclude --innodb-log-file-size=50331648 --backup -H $host -P $port -u $user -p $pass --datadir=$datadir --target-dir=$target 
 
 	endtime=`date +"%Y-%m-%d %H:%M:%S"` 
 	echo "Backup complete at: $endtime (start: $bktime)"
